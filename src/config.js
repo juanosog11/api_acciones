59554e19a18a30d9413e515ef1bf070ccb7a652f
Juan Pablo <juanpa_oso@hotmail.com>
2024-05-18 20:11:37 -0500

    cambio config pero mejor dejar como esta todo

--- src/config.js
+++ src/config.js
@@ -2,12 +2,16 @@ import { config } from 'dotenv'
 
 config()
 
+// export default {
+//     port: process.env.PORT || 3000,
+//     dbuser: process.env.DB_USER || "root",
+//     dbpassword: process.env.DB_PASSWORD,
+//     dbhost: process.env.DB_HOST || "localhost",
+//     dbport: process.env.DB_PORT || "3306",
+//     dbname: process.env.DB_NAME || "Acciones",
+// }
+
 export default {
-    port: process.env.PORT || 3000,
-    dbuser: process.env.DB_USER || "root",
-    dbpassword: process.env.DB_PASSWORD,
-    dbhost: process.env.DB_HOST || "localhost",
-    dbport: process.env.DB_PORT || "3306",
-    dbname: process.env.DB_NAME || "Acciones",
+    url: process.env.DB_URL || "postgres://root:LC6kZ6HHQZbEkcszswesTgqf9f9oIZLD@dpg-cp4juaocmk4c73eldrtg-a/dbacciones"
 }
 
